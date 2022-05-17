658a6988c1ef3eb9f5f97784370810a8a2a94665
avbravo <avbravo@avbravo>
2022-05-17 15:58:13 -0500

    lee microprofile

--- src/main/java/com/avbravo/microjakartanosql/resources/JakartaEE9Resource.java
+++ src/main/java/com/avbravo/microjakartanosql/resources/JakartaEE9Resource.java
@@ -1,8 +1,11 @@
 package com.avbravo.microjakartanosql.resources;
 
+import jakarta.inject.Inject;
+import jakarta.nosql.document.DocumentCollectionManager;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.core.Response;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
 
 /**
  *
@@ -10,11 +13,13 @@ import jakarta.ws.rs.core.Response;
  */
 @Path("jakartaee9")
 public class JakartaEE9Resource {
-    
+       @Inject
+    @ConfigProperty(name = "document")
+        private String document;
     @GET
     public Response ping(){
         return Response
-                .ok("ping Jakarta EE")
+                .ok("ping Jakarta EE" + document)
                 .build();
     }
 }
